5e34bd04248f5a970c5fd373e2233170caa28e11
KaueAntonio <kaue.santos@bandtec.com.br>
2021-10-13 19:27:45 -0300

    Atualização 13/10

--- Site/pt-br/app/controller.js
+++ Site/pt-br/app/controller.js
@@ -4,13 +4,10 @@ const router = express.Router();
 
 router.get('/', (request, response, next) => {
 
-    let sum = ArduinoDataTemp.List.reduce((a, b) => a + b, 0);
-    let average = (sum / ArduinoDataTemp.List.length).toFixed(2);
+    
 
     response.json({
         data: ArduinoDataTemp.List,
-        total: ArduinoDataTemp.List.length,
-        average: isNaN(average) ? 0 : average,
     });
 
 });

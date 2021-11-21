49259ec27b22183957c3685261ecaacca5bdcab4
KaueAntonio <kaue.santos@bandtec.com.br>
2021-11-20 23:33:00 -0300

    Atualização Avaliações

--- Site/API -  SITE/src/models/avaliacaoModel.js
+++ Site/API -  SITE/src/models/avaliacaoModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function listar() {
     console.log("LISTANDO AVALIACOES:");
     var instrucao = `
-    select * from avaliacoes join usuario on idUsuario = fkUsuario join empresa on fkEmpresa = idEmpresa;
+    select num_estrelas as 'estrelinhas', titulo, mensagem, Usuário, empresa.nome as 'nomeempresa', dia, mes, ano from avaliacoes join usuario on idUsuario = fkUsuario join empresa on fkEmpresa = idEmpresa;
     `;
     console.log("Executando a instrução SQL: \n"+instrucao);
     return database.executar(instrucao);

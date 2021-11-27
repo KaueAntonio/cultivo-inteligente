f64234185d5060649250e13c9c470024e8a34395
brunoatavares <bruno.tavares@bandtec.com.br>
2021-11-27 11:08:02 -0300

    Correções

--- Site/API - Arduino/Insert/app/database.js
+++ Site/API - Arduino/Insert/app/database.js
@@ -1,11 +1,28 @@
 var mysql = require('mysql2');
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "cultivo",
-    password: "kaue4991",
-    port: "3306"
+var sql = require('mssql')
+
+var connection = sql.createConnection({
+    user: "wegrowffee",
+    password: "wegrowfe@123",  
+    database: "WeGrowffee",
+    server: "wegrowffee.database.windows.net",
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {  
+        encrypt: true, // for azure
+    }
 });
+
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     database: "cultivo",
+//     password: "kaue4991",
+//     port: "3306"
+// });
 
 connection.connect(function(err) {
     if (err) throw err;
